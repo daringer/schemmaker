bd3c8effc9b61f8a9485c5c3fb3511c04a93760e
Markus Meissner <meissner@em.cs.uni-frankfurt.de>
2014-10-11 18:53:16 +0200

    parser finally fixed, all tets are running fine, woho

--- src/parsers/vhdl.py
+++ src/parsers/vhdl.py
@@ -39,7 +39,24 @@ def parse_vhdl(data_path):
                 if con[-1] == ',':
                     con = con[:-1]
                 block["conns"].append(con)
-            
+             
+            if line.startswith('subnet'):
+                left_res = pat.findall(line)
+                right_res = pat_type.findall(line) 
+                if right_res:
+                    right_res = map(lambda s: s.strip(), right_res)
+                name_res = pat_str_num.findall(left_res[-1])
+                comp_counter.setdefault(name_res[0][0], 0)
+                group_ids = [int(x[-1]) for x in left_res[:-1]]
+                component_id = comp_counter[name_res[0][0]] = comp_counter[name_res[0][0]] + 1
+                
+                block = {'groups': group_ids,
+                         'name': name_res[0][0] + str(component_id),
+                         'type': right_res[0],
+                         'conns': []}
+                p = True
+                
+           
         return blocks
 
 
