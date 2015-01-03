62d661585e21873b6831165a302d109a0f48c1e2
Markus Meissner <markus@evigo.net>
2015-01-03 22:35:33 +0100

    more tests, 330_3 fails :(

--- src/tests/force_algorithm_test.py
+++ src/tests/force_algorithm_test.py
@@ -18,9 +18,6 @@ class ForceAlgorithmUnitTest(unittest.TestCase):
     def setUp(self):
         self.test_data_dir = "../../testdata/"
 
-        self.files = os.listdir(self.test_data_dir)
-        self.files = [x for x in self.files if "324_2" in x]
-
         self.pins = (['vdd'], ['gnd', 'vss'], ['out1'], ['out2'])
 
     # fixture cleanup, called AFTER each test
@@ -41,86 +38,82 @@ class ForceAlgorithmUnitTest(unittest.TestCase):
 
     def construct_field(self, name="test_circ", x=40, y=40):
         self.field = Field("test_circ", x, y)
+ 
+    def _full_simple(self, fn):
+        field = self.construct_field()
+        blocks = self.parse_blocks(os.path.join(self.test_data_dir, fn))
+        f = self.construct_force_algo_obj(self.field, self.blocks, self.pins)
+        cid = fn.split("op")[1].split(".")[0]
+
+        f.step_build(DEBUG)
+        f1 = f.get_debug_field()
+        if DEBUG:
+            fn = draw_field(f1, "schematic_build_{}.pdf".format(cid))
+
+        f.step_initial(DEBUG)
+        f2 = f.get_debug_field()
+        if DEBUG:
+            fn = draw_field(f2, "schematic_init_{}.pdf".format(cid))
+
+        f.step_main(DEBUG)
+        f3 = f.get_debug_field()
+        if DEBUG:
+            fn = draw_field(f3, "schematic_main_{}.pdf".format(cid))
+
+        f.step_last(DEBUG)
+        f4 = f.get_debug_field()
+        if DEBUG:
+            fn = draw_field(f4, "schematic_final_{}.pdf".format(cid))
+
+        # CHECK IF OVERLAPPING BLOCKS WERE FOUND
+        overlap = f4.has_overlapping_blocks()
+        if overlap:
+            f4.show_blocks(sortkey=("groups", "pos"))
+        else:
+            f = f4.to_field()
+            f.optimize_size()
+            f.optimize_block_dirs()
+            r = Routing(f)
+            ret = r.route(4)
+            fn = draw_field(f, "schematic_real_{}.pdf".format(cid))
+
+        self.assertFalse(overlap, "Found overlapping blocks!")
 
 
     ##
     ## tests start here!
     ##
     def test_create_groups(self):
-        for fn in self.files:
-            print "-> create groups - fn: {}".format(fn)
-            field = self.construct_field()
-            blocks = self.parse_blocks(os.path.join(self.test_data_dir, fn))
-            f = self.construct_force_algo_obj(field, blocks, self.pins)
-            f.run(DEBUG)
+        fn = "circuit_bi1_0op324_2.vhdl"
+        print "-> create groups - fn: {}".format(fn)
+        field = self.construct_field()
+        blocks = self.parse_blocks(os.path.join(self.test_data_dir, fn))
+        f = self.construct_force_algo_obj(field, blocks, self.pins)
+        f.run(DEBUG)
 
-            # Check if the main group containts only the two subgroups 0 and 1
-            self.assertEqual(len(f.group_main.childs), 2)
-
+        # Check if the main group containts only the two subgroups 0 and 1
+        self.assertEqual(len(f.group_main.childs), 2)
 
     def test_check_neighbors(self):
-        for fn in self.files:
-            print "-> check neighbors - fn: {}".format(fn)
-            field = self.construct_field()
-            blocks = self.parse_blocks(os.path.join(self.test_data_dir, fn))
-            f = self.construct_force_algo_obj(self.field, self.blocks, self.pins)
-            f.run(DEBUG)
+        fn = "circuit_bi1_0op324_2.vhdl"
+        print "-> check neighbors - fn: {}".format(fn)
+        field = self.construct_field()
+        blocks = self.parse_blocks(os.path.join(self.test_data_dir, fn))
+        f = self.construct_force_algo_obj(self.field, self.blocks, self.pins)
+        f.run(DEBUG)
 
-            # Check that both subgroups of the main group are neighbor
-            self.assertTrue(f.group_main.childs[0].are_neighbor(f.group_main.childs[1]))
+        # Check that both subgroups of the main group are neighbor
+        self.assertTrue(f.group_main.childs[0].are_neighbor(f.group_main.childs[1]))
 
+    def test_full_circ1(self):
+        self._full_simple("circuit_bi1_0op324_2.vhdl")
 
-    def test_full(self):
+    def test_full_circ2(self):
+        self._full_simple("circuit_bi1_0op330_3.vhdl")
 
-        ###
-        # slice the self.files list to reduce test time!!!
-        ###
-        for fn in self.files[:7]:
-            print "-> full run - fn: {}".format(fn)
-            field = self.construct_field()
-            blocks = self.parse_blocks(os.path.join(self.test_data_dir, fn))
-            f = self.construct_force_algo_obj(self.field, self.blocks, self.pins)
-            cid = fn.split("op")[1].split(".")[0]
+    #def test_full_circ3(self):
+    #    self._full_simple("circuit_bi1_0op324_0.vhdl")
 
-
-            f.step_build(DEBUG)
-            f1 = f.get_debug_field()
-            fn = draw_field(f1, "schematic_build_{}.pdf".format(cid))
-
-            f.step_initial(DEBUG)
-            f2 = f.get_debug_field()
-            fn = draw_field(f2, "schematic_init_{}.pdf".format(cid))
-
-            f.step_main(DEBUG)
-            f3 = f.get_debug_field()
-            fn = draw_field(f3, "schematic_main_{}.pdf".format(cid))
-
-            f.step_last(DEBUG)
-            f4 = f.get_debug_field()
-            fn = draw_field(f4, "schematic_final_{}.pdf".format(cid))
-
-            # CHECK IF OVERLAPPING BLOCKS WERE FOUND
-            overlap = f4.has_overlapping_blocks()
-            if overlap:
-                print "-"*80
-                print "#### FAILED ..... FAILED ..... circ_id: {}".format(cid)
-                #f4.show_blocks(sortkey="pos")
-                #f4.show_blocks(sortkey="name")
-                f4.show_blocks(sortkey=("groups", "pos"))
-            else:
-                print "-"*80
-                print "#### {} ####".format(cid)
-                f = f4.to_field()
-                f.optimize_size()
-                f.optimize_block_dirs()
-                #f.expand_field(4)
-                r = Routing(f)
-                ret = r.route(4)
-                fn = draw_field(f, "schematic_real_{}.pdf".format(cid))
-
-
-            # NOT COOL -> removed asserts to see him doing all circuits!
-            #self.assertFalse(overlap, "Found overlapping blocks!")
 
 
 
